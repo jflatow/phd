0b4395ba969d5343b75e87af07298ac3c6e8bb2a
Jared Flatow <jflatow@gmail.com>
2013-04-30 16:57:14 -0700

    stochastic_control: add the trading example; and allow weights to depend on state in MDPs

--- stochastic_control/decision.py
+++ stochastic_control/decision.py
@@ -9,7 +9,7 @@ Our old friend, inventory:
 >>> T = 50
 >>> C = 6
 >>> X = lambda t: range(C + 1)
->>> mdp = MDP(T, X, W=lambda t: {0: .7, 1: .2, 2: .1})
+>>> mdp = MDP(T, X, W=lambda t, x: {0: .7, 1: .2, 2: .1})
 >>> mdp.step = lambda t, x, u, d: x - d + u
 >>> mdp.cost = lambda t, x, u, d: .1 * x + 1 * (u > 0) if 0 <= x - d + u <= C else inf
 >>> mu = dict(mdp.policy())
@@ -23,7 +23,7 @@ class MDP(object):
         self.T = T
         self.X = X
         self.U = U or (lambda t, x: X(t))
-        self.W = W or (lambda t: {None: 1})
+        self.W = W or (lambda t, x: {None: 1})
 
     def step(self, t, x, u, w):
         """Return the state at time t + 1."""
@@ -32,10 +32,10 @@ class MDP(object):
         """Return a real or infinite stage cost."""
 
     def policy(self, p={}):
+        c = lambda t, x, u, W: g(t, x, u, W) + E(t, x, u, W)
         g = lambda t, x, u, W: markov.expected(W, lambda w: self.cost(t, x, u, w), W)
         E = lambda t, x, u, W: markov.expected(W, lambda w: V(t, self.step(t, x, u, w)), W)
         V = lambda t, x: p.get(x, (0, None))[0]
         for t in reversed(xrange(self.T)):
-            W = self.W(t)
-            p = dict((x, min((g(t, x, u, W) + E(t, x, u, W), u) for u in self.U(t, x))) for x in self.X(t))
+            p = dict((x, min((c(t, x, u, self.W(t, x)), u) for u in self.U(t, x))) for x in self.X(t))
             yield t, p
---
+++ stochastic_control/trading.py
@@ -0,0 +1,63 @@
+"""
+Trading example from: http://www.stanford.edu/class/ee365/lectures/trading.pdf
+"""
+import math
+import decision
+
+inf = float('inf')
+cut = lambda v, m, M: min(max(v, m), M)
+
+class Trader(decision.MDP):
+    def __init__(self, T=250, qmin=-5, qmax=15, gamma=.005, N=20):
+        def nprice(p):
+            l = 2 * math.log(p)
+            a = cut(.4 - l, .01, .98)
+            b = cut(.4 + l, .01, .98)
+            return {p * (1 + gamma): a, p / (1 + gamma): b, p: 1 - a - b}
+        prices = set(reduce(lambda a, b: a + [a[-2] / b, a[-1] * b], [1 + gamma] * N, [1, 1]))
+        self.T = T
+        self.X = lambda t: [(q, p) for q in range(qmin, qmax) for p in prices]
+        self.U = lambda t, (q, p): range(qmin - q, qmax - q + 1)
+        self.W = lambda t, (q, p): nprice(p)
+
+    def step(self, t, (q, p), u, p_):
+        return q + u, p_
+
+    def cost(self, t, (q, p), u, p_):
+        if t < self.T - 1:
+            return u * p
+        return 0 if q + u == 0 else inf
+
+class TraderSH(Trader):
+    def cost(self, t, (q, p), u, p_):
+        if t < self.T - 1:
+            return u * p + (.005 if q < 0 else 0)
+        return 0 if q + u == 0 else inf
+
+class TraderLin(Trader):
+    def cost(self, t, (q, p), u, p_):
+        if t < self.T - 1:
+            return u * p + (.005 * abs(u))
+        return 0 if q + u == 0 else inf
+
+class TraderSHNL(Trader):
+    def cost(self, t, (q, p), u, p_):
+        if t < self.T - 1:
+            return u * p + (.0001 if q < 0 else 0) + (.005 * abs(u) ** 1.5)
+        return 0 if q + u == 0 else inf
+
+def plot(policy, T=0):
+    import mpl_toolkits.mplot3d
+    import pylab
+    o = policy[T]
+    f = pylab.figure()
+    a = f.add_subplot(111, projection='3d')
+    a.plot_trisurf([q for q, _ in o], [p for _, p in o], [u for v, u in o.values()], cmap=pylab.cm.jet)
+    a.set_xlabel('Quantity')
+    a.set_ylabel('Price')
+    a.set_zlabel('Action')
+    pylab.show()
+
+if __name__ == '__main__':
+    for trader in (Trader, TraderSH, TraderLin, TraderSHNL):
+        plot(dict(trader(T=10).policy()))
